6608f95198790f3bd47e8948e4323c2905aefcad
Harmaton <njagiiharmaton@gmail.com>
2023-03-31 12:23:18 +0300

    keys

--- src/app/api/auth/[...nextauth].js
+++ src/app/api/auth/[...nextauth].js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import InstagramProvider from "next-auth/providers/instagram";
 
+
 export const authOptions = {
   // Configure one or more authentication providers
   providers: [
